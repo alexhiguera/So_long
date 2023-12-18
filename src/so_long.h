b3acb8c15d2d984073be3a0b0731968c4f130b57
alexhiguera <ahiguera@student.42urduliz.com>
2023-12-18 17:53:06 +0100

    update: open window test its ok

--- src/so_long.h
+++ src/so_long.h
@@ -6,14 +6,14 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 12:16:32 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/12/13 17:06:10 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/12/18 16:46:15 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "./includes/libft/libft.h"
-# include "./includes/minilibx/mlx.h"
+//# include "./includes/libft/libft.h"
+# include "../includes/minilibx/mlx.h"
 
 #endif
