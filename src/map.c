973ca5c2a069a07db9dd1acd71027f0af1ce8a07
alexhiguera <ahiguera@student.42urduliz.com>
2023-12-19 17:48:32 +0100

    first step to create map

--- src/map.c
+++ src/map.c
@@ -1,26 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 12:16:32 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/12/19 17:11:04 by ahiguera         ###   ########.fr       */
+/*   Created: 2023/12/19 17:09:53 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/12/19 17:47:14 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "../src/so_long.h"
 
-//# include "./includes/libft/libft.h"
-# include "../includes/minilibx/mlx.h"
-
-typedef struct s_map
+/**
+ * Prints the map to the standard output.
+ *
+ * @param map The map structure containing the map data.
+ */
+void	ft_print_map(t_map *map)
 {
-	char	**map;
-	int		rows;
-	int		cols;
-}				t_map;
+	int row_len;
+	int cols_len;
 
-#endif
+	row_len = 0;
+	while (row_len < map->rows)
+	{
+		cols_len = 0;
+		while (cols_len < map->cols)
+		{
+			ft_putchar_fd(map->map[row_len][cols_len], 1);
+			cols_len++;
+		}
+		ft_putchar_fd('\n', 1);
+		row_len++;
+	}
+}
