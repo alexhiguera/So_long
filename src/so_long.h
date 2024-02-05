77014eb1eadde17de5789df6244c7e66d6200e45
alexhiguera <ahiguera@student.42urduliz.com>
2024-02-05 11:15:19 +0100

    update: cambio ruta de libft

--- src/so_long.h
+++ src/so_long.h
@@ -6,15 +6,18 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 12:16:32 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/12/19 17:11:04 by ahiguera         ###   ########.fr       */
+/*   Updated: 2024/01/30 16:34:31 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-//# include "./includes/libft/libft.h"
+# include "../includes/Libft_2.0/src/libft.h"
 # include "../includes/minilibx/mlx.h"
+
+# include <fcntl.h>
+# include <stdio.h>
 
 typedef struct s_map
 {
