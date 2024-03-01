eeb299e9c911bb672f2bcdbfbf746a386a121a8f
alexhiguera <alexhigueramartinez@gmail.com>
2024-03-01 18:05:03 +0100

    build: so_long.h for mac and linux

---
+++ src_linux/so_long.h
@@ -0,0 +1,27 @@
+//TODO: add header
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../includes/mlx_linux/mlx.h"
+# include "../includes/Libft_2.0/src/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define WINDOW_WIDTH	64
+# define WINDOW_HEIGHT	64
+
+//hooks
+# define KEY_ESC 53
+# define KEY_W 13
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_A 0
+# define KEY_UP 126
+# define KEY_DOWN 125
+# define KEY_RIGHT 124
+# define KEY_LEFT 123
+
+#endif
+
--- src_macos/so_long.h
+++ src_macos/so_long.h
@@ -3,27 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*   By: columbux <columbux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 12:16:32 by ahiguera          #+#    #+#             */
-/*   Updated: 2024/01/30 16:34:31 by ahiguera         ###   ########.fr       */
+/*   Updated: 2024/03/01 18:03:12 by columbux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//TODO: change header
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include "../includes/mlx_macos/mlx.h"
 # include "../includes/Libft_2.0/src/libft.h"
-# include "../includes/minilibx/mlx.h"
-
+# include <stdlib.h>
+# include <unistd.h>
 # include <fcntl.h>
-# include <stdio.h>
 
-typedef struct s_map
-{
-	char	**map;
-	int		rows;
-	int		cols;
-}				t_map;
+# define WINDOW_WIDTH	64
+# define WINDOW_HEIGHT	64
+
+//hooks
+# define KEY_ESC 53
+# define KEY_W 13
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_A 0
+# define KEY_UP 126
+# define KEY_DOWN 125
+# define KEY_RIGHT 124
+# define KEY_LEFT 123
 
 #endif
