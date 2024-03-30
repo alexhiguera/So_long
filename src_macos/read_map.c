2a4751ede04866874ed74bc407021320a0314775
Columbux <ahiguera@student.42urduliz.com>
2024-03-31 00:11:33 +0100

    update: refactor

--- src_macos/read_map.c
+++ src_macos/read_map.c
@@ -6,11 +6,46 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 17:09:53 by ahiguera          #+#    #+#             */
-/*   Updated: 2024/03/30 23:54:51 by alex             ###   ########.fr       */
+/*   Updated: 2024/03/31 00:06:33 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static void	check_other_things(t_game *game, int y, int x)
+{
+	if (game->map.map[y][x] != '1' && game->map.map[y][x] != '0'
+		&& game->map.map[y][x] != '\n')
+		so_error("Error\nChar on map Invalid\n");
+}
+
+void	so_count_things(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (game->map.map[y])
+	{
+		x = 0;
+		while (game->map.map[y][x])
+		{
+			if (game->map.map[y][x] == 'P')
+				game->map.player++;
+			else if (game->map.map[y][x] == 'C')
+				game->player.c_collects++;
+			else if (game->map.map[y][x] == 'E')
+				game->map.count++;
+			else
+				check_other_things(game, y, x);
+			x++;
+		}
+		y++;
+	}
+	if (game->map.player != 1 || game->map.count != 1
+		|| game->player.c_collects < 1 || game->map.count != 1)
+		so_error("Error\nInvalid map\n");
+}
 
 void	so_read_map(t_game *game)
 {
@@ -57,39 +92,4 @@ void	so_check_perimeter(t_game *game)
 			- 1] != '1')
 			so_error("Error\nMap is not closed\n");
 	}
-}
-
-void	check_other_things(t_game *game, int y, int x)
-{
-	if (game->map.map[y][x] != '1' && game->map.map[y][x] != '0'
-		&& game->map.map[y][x] != '\n')
-		so_error("Error\nChar on map Invalid\n");
-}
-
-void	so_count_things(t_game *game)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	while (game->map.map[y])
-	{
-		x = 0;
-		while (game->map.map[y][x])
-		{
-			if (game->map.map[y][x] == 'P')
-				game->map.player++;
-			else if (game->map.map[y][x] == 'C')
-				game->player.c_collects++;
-			else if (game->map.map[y][x] == 'E')
-				game->map.count++;
-			else
-				check_other_things(game, y, x);
-			x++;
-		}
-		y++;
-	}
-	if (game->map.player != 1 || game->map.count != 1
-		|| game->player.c_collects < 1 || game->map.count != 1)
-		so_error("Error\nInvalid map\n");
 }
