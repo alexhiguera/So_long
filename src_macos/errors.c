f489a8b598ccac3e032492835a2039799a22aa48
Columbux <ahiguera@student.42urduliz.com>
2024-04-06 02:12:55 +0200

    update: so_error message red color

--- src_macos/errors.c
+++ src_macos/errors.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 17:09:53 by ahiguera          #+#    #+#             */
-/*   Updated: 2024/04/04 16:52:33 by alex             ###   ########.fr       */
+/*   Updated: 2024/04/06 02:10:55 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,12 @@ void	so_handler_errors(t_game *game)
 {
 	if (game->player.c_collects != game->player.tokens
 		|| game->player.exit == false)
-		so_error("Map cant played");
+		so_error("Map cant played\n");
 	so_free(game);
 }
 
 void	so_error(char *message)
 {
-	ft_printf("Error: %s\n", message);
+	ft_printf("\033[31mError: %s\n", message);
 	exit(EXIT_FAILURE);
 }
